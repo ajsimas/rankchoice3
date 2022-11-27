598bcea1b199d48f717bd0dcc24b5e79c1754432
Austin Simas <austin@simas.io>
2022-11-27 13:15:03 -0700

    initial module config

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const express = require('express')
+// import modules
+const express = require('express');
+const engine = require('ejs-mate');
+
+// initialize express
+const app = express();
+app.use(express.static('public'));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('homepage');
+});
+
+app.listen(8080, () => {});
