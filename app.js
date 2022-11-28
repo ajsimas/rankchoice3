ae6fbc2b4dcd67b74f6ba58fb5074a337c6b03ba
Austin Simas <austin@simas.io>
2022-11-27 21:06:16 -0700

    initial config of tedious

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const engine = require('ejs-mate');
 const rankchoice = require('./rankchoice.js');
+const Connection = require('tedious').Connection;
+const Request = require('tedious').Request;
 
 // initialize express
 const app = express();
@@ -10,8 +12,33 @@ app.use(express.urlencoded({extended: true}));
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
+// initialize tedious
+const config = {
+  server: 'localhost',
+  authentication: {
+    type: 'default',
+    options: {
+      userName: 'node',
+      password: 'Changeme12345',
+    },
+  },
+  options: {
+    database: 'rankchoice',
+    trustServerCertificate: true,
+    rowCollectionOnRequestCompletion: true,
+  },
+};
+const connection = new Connection(config);
+connection.on('connect', function(err) {
+  if (err) console.log(err);
+  else console.log('Connected');
+});
+
+// Express routes
+
 app.get('/', (req, res) => res.render('homepage'));
 app.get('/poll/create', (req, res) => res.render('create'));
 app.post('/poll/create', (req, res) => rankchoice.createPoll(req));
 
-app.listen(8080, () => {});
+app.listen(8080, () => { });
+connection.connect();
