f2e69fb1c488cda0795f0e5f1385bcaadb9e50cd
Austin Simas <austin@simas.io>
2022-12-13 14:53:39 -0700

    RN-25 add flash functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const engine = require('ejs-mate');
 const {mySession} = require('./express-session');
+const flash = require('connect-flash');
 
 // initialize express
 const app = express();
@@ -9,6 +10,7 @@ const router = require('./routes/index');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(mySession);
+app.use(flash());
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.use('/', router);
